d34d5503819d4d6b45f38540e1df5c209363d479
Varun M S <varun.cse.nitap@gmail.com>
2017-08-20 19:08:05 +0530

    Update AutoLinkedinInviter.js

--- AutoLinkedinInviter.js
+++ AutoLinkedinInviter.js
@@ -9,11 +9,32 @@
 // @include     invites
 // @version     1
 // @grant       none
+// @run-at      document-idle
 // ==/UserScript==
 
-people = document.getElementsByClassName("button-secondary-small");
+  
 
-for(person = 0; person < people.length ; person++){
+window.setTimeout(function() {
+                  
+                  
+                  
+                  var people = document.getElementsByClassName("button-secondary-small");
 
-  people[person].click();
-}
+                  for(person = 0; person < people.length ; person++){
+                    people[person].click();
+                  }
+                  console.log("lmera");
+                  alert("ffdsfsdf");
+
+                  
+                  location.reload();
+                  
+                  
+                  
+                  
+                  },1000)
+  
+  
+
+
+
